c0e61824c64974974e2050fcc1b5ed89a5e4b809
baine101 <alex.waine@juicymedia.co.uk>
2016-06-09 23:20:05 +0100

    file structure

---
+++ app/code/Juicy/Geoip/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 09/06/2016
+ * Time: 10:23
+ */
+use \Magento\Framework\Component\ComponentRegistrar;
+
+        ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Juicy_Geoip', __DIR__);
+
+
+
+
