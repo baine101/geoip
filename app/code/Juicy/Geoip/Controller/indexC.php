fdee37dcbbfa023b90a7c860492c97e455a7ac4a
baine101 <alex.waine@juicymedia.co.uk>
2016-06-13 23:05:26 +0100

    file

---
+++ app/code/Juicy/Geoip/Controller/indexC.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baine101
+ * Date: 13/06/16
+ * Time: 22:59
+ */
---
+++ app/code/Juicy/Geoip/Observer/Observer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Juicy\Geoip\Observer;
+
+
+class Observer
+{
+    protected $_geoipData;
+    protected $_registry = null;
+
+    public function __construct(\Juicy\Geoip\Helper\Data $geoipData, \Magento\Framework\Registry $registry
+    )
+    {
+        $this->_geoipData = $geoipData;
+        $this->_registry = $registry;
+    }
+
+    public function geoipStart(\Magento\Framework\Event\Observer $observer)
+    {
+
+    }
+
+
+}
